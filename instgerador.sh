2c64bc6167172c804c90013d05a5c3e9bfbbcf7a
GILILLOMX <46901903+GILILLOMX@users.noreply.github.com>
2019-01-23 14:35:46 -0700

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -59,7 +59,7 @@ echo -e "\033[1;31m- \033[1;32mRecebido Com Sucesso!"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
 } || echo -e "\033[1;31m- \033[1;31mFalha (nao recebido!)"
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/GILILLOMX/GENERADOR-NEW-ADM-ORIGINAL/master/Install/trans &> /dev/null
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/GILILLOMX/GENERADOR-NEW-ADM-GILILLOMX/master/Install/trans &> /dev/null
 [[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
 [[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
