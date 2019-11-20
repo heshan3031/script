10782d0c7b0615a57f052280fbd3a28595363025
GililloGonz4 <49882794+GililloGonz4@users.noreply.github.com>
2019-11-19 20:54:24 -0700

    Add files via upload

--- gerador/ssl.sh
+++ gerador/ssl.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
-SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
-SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
-API_TRANS="aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy9sNmlxZjV4anRqbXBkeDUvdHJhbnM/ZGw9MA=="
+barra="\033[0m\e[31m=================================================\033[1;37m"
+fun_bar () {
+comando="$1"
+ _=$(
+$comando > /dev/null 2>&1
+) & > /dev/null
+pid=$!
+while [[ -d /proc/$pid ]]; do
+echo -ne " \033[1;33m["
+   for((i=0; i<10; i++)); do
+   echo -ne "\033[1;31m**"
+   sleep 0.2
+   done
+echo -ne "\033[1;33m]"
+sleep 1s
+echo
+tput cuu1 && tput dl1
+done
+echo -e " \033[1;33m[\033[1;31m********************\033[1;33m] - \033[1;32m100%\033[0m"
+sleep 1s
+}
+API_TRANS="aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy9sZnlxZGI5NnJkejl5bW8vdHJhbnM/ZGw9MA=="
 SUB_DOM='base64 -d'
 wget -O /usr/bin/trans $(echo $API_TRANS|$SUB_DOM) &> /dev/null
 mportas () {
@@ -17,51 +36,44 @@ echo -e "$portas"
 }
 ssl_stunel () {
 [[ $(mportas|grep stunnel4|head -1) ]] && {
-msg -ama " $(fun_trans "Parando Stunnel")"
-msg -bar
-fun_bar "apt-get purge stunnel4 -y"
-msg -bar
-msg -ama " $(fun_trans "Parado Com Sucesso!")"
-msg -bar
+echo -e "${cor[2]}Parando Stunnel"
+echo -e "$barra"
+fun_bar "service stunnel4 stop"
+echo -e "$barra"
+echo -e "${cor[2]}Parado Con exito!"
+echo -e "$barra"
 return 0
 }
-msg -azu " $(fun_trans "SSL Stunnel")"
-msg -bar
-msg -ama " $(fun_trans "Selecione Uma Porta De Redirecionamento Interna")"
-msg -ama " $(fun_trans "Ou seja, uma Porta no Seu Servidor Para o SSL")"
-msg -bar
-         while true; do
-         echo -ne "\033[1;37m"
-         read -p " Local-Port: " portx
-         if [[ ! -z $portx ]]; then
-             if [[ $(echo $portx|grep [0-9]) ]]; then
-                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans "Porta Invalida")"
-             fi
-         fi
-         done
-msg -bar
-DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
-msg -ama " $(fun_trans "Agora Presizamos Saber Qual Porta o SSL, Vai Escutar")"
-msg -bar
+echo -e "${cor[3]}Que puerto desea abrir como SSL Openssh"
+echo -e "$barra"
     while true; do
-    read -p " Listen-SSL: " SSLPORT
+    read -p " Puerto SSL: " SSLPORT
     [[ $(mportas|grep -w "$SSLPORT") ]] || break
-    echo -e "\033[1;33m $(fun_trans "esta Porta Ja esta em Uso")"
+    echo -e "${cor[3]}esta puerta está en uso"
     unset SSLPORT
     done
-msg -bar
-msg -ama " $(fun_trans "Instalando SSL")"
-msg -bar
+echo -e "$barra"
+echo -e "${cor[4]}Instalando SSL"
+echo -e "$barra"
 fun_bar "apt-get install stunnel4 -y"
-echo -e "cert = /etc/stunnel/stunnel.pem\nclient = no\nsocket = a:SO_REUSEADDR=1\nsocket = l:TCP_NODELAY=1\nsocket = r:TCP_NODELAY=1\n\n[stunnel]\nconnect = 127.0.0.1:${DPORT}\naccept = ${SSLPORT}" > /etc/stunnel/stunnel.conf
-openssl genrsa -out key.pem 2048 > /dev/null 2>&1
-(echo br; echo br; echo uss; echo speed; echo adm; echo ultimate; echo @admultimate)|openssl req -new -x509 -key key.pem -out cert.pem -days 1095 > /dev/null 2>&1
-cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+echo -e "$barra"
+echo -e "${cor[4]}Presione Enter a todas las opciones"
+sleep 3
+echo -e "$barra"
+openssl genrsa 2048 > stunnel.key
+openssl req -new -key stunnel.key -x509 -days 1094 -out stunnel.crt
+cat stunnel.crt stunnel.key > stunnel.pem
+mv stunnel.pem /etc/stunnel/
+echo -e "client = no\n[ssh]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:22" > /etc/stunnel/stunnel.conf
+
+echo "ENABLED=1 " >> /etc/default/stunnel4
+echo "FILES="/etc/stunnel/*.conf" " >> /etc/default/stunnel4
+echo "OPTIONS="" " >> /etc/default/stunnel4
+echo "PPP_RESTART=0" >> /etc/default/stunnel4
 service stunnel4 restart > /dev/null 2>&1
-msg -bar
-msg -ama " $(fun_trans "INSTALADO COM SUCESSO")"
-msg -bar
+echo -e "$barra"
+echo -e "${cor[1]}INSTALADO CON EXITO"
+echo -e "$barra"
 return 0
 }
 ssl_stunel
