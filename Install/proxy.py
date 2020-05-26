ec23e89011eeaea63f15711ef3eade6790bb3365
GililloGonz4 <49882794+GililloGonz4@users.noreply.github.com>
2020-05-26 13:32:43 -0600

    Add files via upload

--- Install/proxy.py
+++ Install/proxy.py
@@ -17,7 +17,7 @@ MSG = 'By@Alexmod80'
 COR = '<font color="null">'
 FTAG = '</font>'
 DEFAULT_HOST = '127.0.0.1:22'
-RESPONSE = "HTTP/1.1 200 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
+RESPONSE = "HTTP/1.1 200 " + str(COR) + str(MSG) + str(FTAG) + "\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 !!!conexion exitosa!!!\r\n\r\n"
  
 class Server(threading.Thread):
     def __init__(self, host, port):
