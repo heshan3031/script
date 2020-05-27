7130a5fc6b2a532df3085ba504f50f14f70e29b4
GililloGonz4 <49882794+GililloGonz4@users.noreply.github.com>
2020-05-27 12:44:25 -0600

    Update notfy.sh

--- Install/notfy.sh
+++ Install/notfy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-NOM=`less /root/nombre.log`
+NOM=`less /etc/newadm/ger-user/nombre.log`
 NOM1=`echo $NOM`
 notify -i "⚠️ LA VPS: $NOM1 FUE REINICIANDA ⚠️" -t "❗️El Reinicio fue ✅EXITOSO✅❗️"
 
