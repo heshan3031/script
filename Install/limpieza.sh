5bb86aff73fcfa358b45c1ed29e8dabda8e11057
GililloGonz4 <49882794+GililloGonz4@users.noreply.github.com>
2020-07-13 08:27:27 -0600

    Update limpieza.sh

--- Install/limpieza.sh
+++ Install/limpieza.sh
@@ -1,8 +1,12 @@
+#script/install
+#limpieza automatica alas 5 am empieza la limpieza automatica
 echo "Limpiando sistema y Reiniciando Servicios" 
 echo 3 > /proc/sys/vm/drop_caches &> /dev/null
 sysctl -w vm.drop_caches=3 &> /dev/null
 swapoff -a && swapon -a &> /dev/null
+#reiniciando servicios
 service ssh restart > /dev/null 2>&1
 service squid restart > /dev/null 2>&1
 service squid3 restart >/dev/null 2>&1
 echo "Limpieza Finalizada"
+#finalizada
