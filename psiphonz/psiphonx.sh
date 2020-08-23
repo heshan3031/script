525ae66ff568c715ccc608f5fb755e1a65321eeb
scriptsmx <49882794+scriptsmx@users.noreply.github.com>
2020-08-23 15:13:25 -0600

    Update psiphonx.sh

--- psiphonz/psiphonx.sh
+++ psiphonz/psiphonx.sh
@@ -5,6 +5,7 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 [[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
 }
 fun_ip
+rm -rf openssl-1.0.2l.tar.gz openssl-1.0.2l psiphonx.sh
 echo -e "\e[1;37m"
 echo " INICIANDO LA INSTALACION"
 sudo apt-get install python-pip
@@ -19,15 +20,15 @@ make install
 sudo apt-get install libssl-dev
 sleep 2s
 wget https://raw.githubusercontent.com/Psiphon-Labs/psiphon-tunnel-core-binaries/master/psiphond/psiphond
-chmod 555 psiphond
+chmod +x psiphond
 sleep 3s
 ./psiphond --ipaddress 0.0.0.0 --web 8625 --protocol SSH:3001 --protocol OSSH:3002 --protocol FRONTED-MEEK-OSSH:443 generate
 sleep 4s
-chmod 666 psiphond.config
-chmod 666 psiphond-traffic-rules.config
-chmod 666 psiphond-osl.config
-chmod 666 psiphond-tactics.config
-chmod 666 server-entry.dat/
+chmod +x psiphond.config
+chmod +x psiphond-traffic-rules.config
+chmod +x psiphond-osl.config
+chmod +x psiphond-tactics.config
+chmod +x server-entry.dat/
 sleep 5s
 echo "INSTALADO CON EXITO"
 rm -rf openssl-1.0.2l.tar.gz
